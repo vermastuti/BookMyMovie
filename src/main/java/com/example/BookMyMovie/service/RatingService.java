0bce45d59a6eb2b0542be99763f928e72eb9a371
sangeetapandey <sangeeta.pandey@thoughtworks.com>
2025-10-22 17:26:39 +0530

    Added changes in MovieRating

--- src/main/java/com/example/BookMyMovie/service/RatingService.java
+++ src/main/java/com/example/BookMyMovie/service/RatingService.java
@@ -2,23 +2,76 @@ package com.example.BookMyMovie.service;
 
 //package com.example.MovieRating.service;
 //import com.example.MovieRating.model.Rating;
+//import com.example.MovieRating.repository.RatingRepository;
 
 import com.example.BookMyMovie.model.Rating;
+import com.example.BookMyMovie.repository.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RatingService implements iRatingService {
 
 
-public interface RatingService {
+    @Autowired
+    RatingRepository ratingRepository;
 
-    Rating addReview(Rating rating);
+    @Override
+    public Rating addReview(Rating rating) {
 
-    List<Rating> viewAllRatings();
+        Rating ratingcreated = ratingRepository.save(rating);
 
-    List<Rating> viewAllRatingsByCutomerId(Integer customerId);
+        return ratingcreated;
 
-    List<Rating> viewAllRatingsByMovieId(Integer movieId);
+    }
 
-    Rating updateRating(Integer ratingId , Integer rating , Integer customerId , String newReview);
 
+    @Override
+    public List<Rating> viewAllRatings() {
+
+        List<Rating> ratings = ratingRepository.findAll();
+
+        return ratings;
+
+    }
+
+
+    @Override
+    public List<Rating> viewAllRatingsByCutomerId(Integer customerId) {
+
+        List<Rating> ratingListByCustomerId = ratingRepository.findByCustomerId(customerId);
+
+        //ratingListByCustomerId.stream().forEach((c)-> c.toString());
+        return ratingListByCustomerId;
+    }
+
+
+    //yet to implement
+    @Override
+    public List<Rating> viewAllRatingsByMovieId(Integer movieId) {
+
+        List<Rating> ratingsByMovieId = ratingRepository.findByMovieId(movieId);
+
+
+        return ratingsByMovieId;
+    }
+
+    public Rating updateRating(Integer ratingId, Integer customerId, Integer newRating, String newReview) {
+
+        Rating rating = ratingRepository.findById(ratingId)
+                .orElseThrow(() -> new RuntimeException("Rating not found with given Id ratingId : " + ratingId));
+
+        // Update fields
+        rating.setReview(newReview);
+        rating.setRating(newRating);
+
+        return ratingRepository.save(rating);
+    }
 
 }
+
+
+
